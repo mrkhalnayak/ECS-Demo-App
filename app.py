0df1c8a0b938fef09d1f029aba54d4f13d46d066
Shriyanshu <shriyanshu@hotmail.com>
2025-07-01 17:48:23 +0530

    new changed made in the code app.py file

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello I am Shri, This application is created on Flask!"
 
+@app.route('/home')
+def hello():
+    return "Python-App"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
 
